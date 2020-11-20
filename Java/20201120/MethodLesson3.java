f27d31ca29c949d80003125f00b3f84f364d0231
Ntakeda <muzat.yakuzajeep@gmail.com>
2020-11-20 12:49:28 +0900

    メソッドファイル位置修正

---
+++ Java/20201120/MethodLesson3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class MethodLesson3{
+	public static void main(String[] args){
+		sumOf(3,8);
+		sumOf(100,200);
+	}
+	static void sumOf(int a,int b){
+		System.out.printf("%dと%dの和は%dです%n",a,b,a+b);
+	}
+}
