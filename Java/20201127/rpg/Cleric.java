bacd0da0f28c1fb89672eb5ab304cbfacda2e8b9
Ntakeda <muzat_yakuzajeep@yahoo.co.jp>
2020-11-27 11:07:06 +0900

    winにて27日授業

---
+++ Java/20201127/rpg/Cleric.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Cleric{
+	String name;
+	int hp=50;
+	final int MAX_HP=50;
+	int mp=10;
+	final int MAX_MP=10;
+
+	public void selfAid(){
+		this.mp-=5;
+		this.hp=MAX_HP;
+	}
+	public int pray(int sec){
+		int temp=new Random().nextInt(3)+sec;
+		int real;
+		/*if(this.mp+temp>this.MAX_MP){
+			real=this.MAX_MP-this.mp;
+			this.mp=this.MAX_MP;
+		}else{
+			real=temp;
+		}*/
+		real=this.mp+temp > this.MAX_MP ? this.MAX_MP-this.mp : temp;
+		this.mp+=real;
+		return real;
+	}
+}
---
+++ Java/20201127/rpg/Hero.java
@@ -0,0 +1,24 @@
+public class Hero{
+	String name;
+	int hp=50;
+
+	public void sleep(){
+		this.hp=100;
+		System.out.println(this.name + "は、眠って回復した！");
+	}
+	public void sit(int sec){
+		this.hp+=sec;
+		System.out.println(this.name+"は、"+sec+"秒座った！");
+		System.out.println("HPが"+sec+"ポイント回復した");
+	}
+	public void slip(){
+		this.hp-=5;
+		System.out.println(this.name+"は毒をうけた！");
+		System.out.println(this.name+"に5のダメージ！");
+	}
+	public void run(){
+		System.out.println(this.name+"は逃げだした！");
+		System.out.println("GameOver");
+		System.out.println("最終HPは"+this.hp+"でした。");
+	}
+}
